226bc70435ac19bce088805d509a3471db193689
MORGANJASON <420morganjason@gmail.com>
2023-03-27 22:37:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+const submitData = {
+  userName: "Morgan",
+  userEmail: "420morganjason@gmail.com",
+};
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(submitData),
+};
+
+fetch("http://localhost:3000/dogs", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  })
+   .catch(function (error) {
+    alert("Vitu Mbaya! Toka Hapo!");
+    console.log(error.alert());
+  });
